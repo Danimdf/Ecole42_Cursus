4a1dbd7f5182b3b6adaabf6dc280399717c4c84c
Daniela Monteiro <danimdf360@gmail.com>
2021-09-10 13:22:19 -0300

    add valid object

--- so_long/ft_valid.c
+++ so_long/ft_valid.c
@@ -6,13 +6,22 @@
 /*   By: dmonteir <dmonteir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/10 11:08:07 by dmonteir          #+#    #+#             */
-/*   Updated: 2021/09/10 11:09:04 by dmonteir         ###   ########.fr       */
+/*   Updated: 2021/09/10 11:36:48 by dmonteir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
+int	valid_Char()
+
 int	ft_valid(char **map, t_game *game)
 {
+	int	x;
+	int	y;
 
+	x = game->obj->x;
+	y = game->obj->y;
+
+	if(!valid_char(map[x][y]))
+		return (0);
 }
